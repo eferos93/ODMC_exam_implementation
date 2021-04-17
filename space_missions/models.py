06176ef0a6db68644725ec9c39a7b016e54ea016
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-17 18:47:15 +0200

    fixing according to the uml model

--- space_missions/models.py
+++ space_missions/models.py
@@ -1,16 +1,17 @@
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 # Create your models here.
 
 class Country(models.Model):
-    code = models.CharField(max_length=2)
+    code = models.CharField(max_length=2, primary_key=True)
     name = models.CharField(max_length=40)
     continent = models.TextChoices('Continent', 'Africa Americas Asia Europe Oceania')
 
 
 class Astronaut(models.Model):
+    id = models.CharField(max_length=256, primary_key=True)
     name = models.CharField(max_length=40)
     original_name = models.CharField(max_length=40)
 
@@ -32,36 +33,47 @@ class Astronaut(models.Model):
 
 class Selection(models.Model):
     # NB: Una missione può avere + programmi di selezione
-    name = models.CharField(max_length=40)
-    missionname = models.ForeignKey('space_missions.Mission', on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, primary_key=True)
+    missions = models.ManyToManyField('space_missions.Mission', on_delete=models.CASCADE)
+    astronauts = models.ManyToManyField('space_missions.Astronaut',
+                                        through='RecruitmentProcedure',
+                                        related_name='selections',
+                                        on_delete=models.CASCADE)
 
 
 class RecruitmentProcedure(models.Model):
-    astronautID = models.ForeignKey('space_missions.Astronaut', on_delete=models.CASCADE)
-    selectionID = models.ForeignKey('space_missions.Selection', on_delete=models.CASCADE)
+    class Meta:
+        unique_together = [('astronaut', 'selection')]
+    astronaut = models.ForeignKey('space_missions.Astronaut', on_delete=models.CASCADE)
+    selection = models.ForeignKey('space_missions.Selection', on_delete=models.CASCADE)
     year_of_selection = models.PositiveSmallIntegerField()
 
 
 class AstronautOccupation(models.Model):
-    astronautID = models.ForeignKey('space_missions.Astronaut', on_delete=models.CASCADE)
-    mission_title = models.ForeignKey('space_missions.Mission', on_delete=models.CASCADE)
-    role_for_mission = models.CharField(max_length=40)
+    class Meta:
+        unique_together = [('astronaut', 'mission')]
+    astronaut = models.ForeignKey('space_missions.Astronaut', on_delete=models.CASCADE)
+    mission = models.ForeignKey('space_missions.Mission', on_delete=models.CASCADE)
+    role = models.CharField(max_length=40)
     year_of_join = models.PositiveSmallIntegerField()
 
 
-class Launches(models.Model):
+class Launch(models.Model):
     SF_choices = ('Success', 'Fail', 'unknown')
-    TypeOfLaunch_choices = ('Orbital', 'DeepSpaceMission', 'unknown')
+    type_of_launch_choices = ('Orbital', 'DeepSpaceMission', 'unknown')
 
-    LaunchID = models.CharField(max_length=10)
-    Date = models.DateField()
-    LaunchVehicle = models.ForeignKey('space_missions.LaunchVehicle', on_delete=models.CASCADE)
-    MissionID = models.ForeignKey('space_missions.Mission', on_delete=models.CASCADE)
-    Organization = models.ForeignKey('space_missions.Organization', on_delete=models.CASCADE)
-    SuccessOrFail = models.CharField(max_length=20, choices=[(d, d) for d in SF_choices])
-    TypeOfLaunch = models.CharField(max_length=20, choices=[(d, d) for d in TypeOfLaunch_choices])
+    id = models.CharField(max_length=10, primary_key=True)
+    date = models.DateField()
+    launch_vehicle = models.ForeignKey('space_missions.LaunchVehicle', on_delete=models.CASCADE)
+    organization = models.ForeignKey('space_missions.Organization', on_delete=models.CASCADE)
+    success_or_fail = models.CharField(max_length=20, choices=[(d, d) for d in SF_choices])
+    type_of_launch = models.CharField(max_length=20, choices=[(d, d) for d in type_of_launch_choices])
 
 
 class Mission(models.Model):
-    LaunchID = models.ForeignKey(Launches, on_delete=models.CASCADE)
-    MissionID = models.CharField(max_length=30)
+    launch = models.OneToOneField('space_missions.models.Launch', on_delete=models.CASCADE)
+    name = models.CharField(max_length=30, primary_key=True)
+    astronauts = models.ManyToManyField('space_missions.Astronaut',
+                                        on_delete=models.CASCADE,
+                                        through='AstronautOccupation',
+                                        related_name='missions')
