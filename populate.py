2d7442a4aa1f66461983e4e7fe637f4e2afae9e1
eferos93 <eros.fabrici@gmail.com>
2021-04-21 16:11:01 +0200

    models registered

--- populate.py
+++ populate.py
@@ -1,4 +1,20 @@
 import os
 import django
+import pandas as pd
+
+from space_missions.models import Country
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'models.settings')
 django.setup()
+
+
+# COUNTRY
+def create_country(fields):
+    return Country(code=fields[0], name=fields[1], continent=fields[2])
+
+
+def collapse_rows_to_list(data_frame):
+    return pd.Series(data_frame.values.tolist())
+
+
+Country.objects.bulk_create(list(map(create_country, collapse_rows_to_list(pd.read_csv('Data/Country.csv')))))
--- space_missions/admin.py
+++ space_missions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Country)
+admin.site.register(models.AstronautSelection)
+admin.site.register(models.Astronaut)
+admin.site.register(models.AstronautOccupation)
+admin.site.register(models.Coordinate)
+admin.site.register(models.Launch)
+admin.site.register(models.LaunchVehicle)
+admin.site.register(models.Measure)
+admin.site.register(models.Mission)
+admin.site.register(models.Organisation)
+admin.site.register(models.Selection)
+admin.site.register(models.Stage)
+admin.site.register(models.VehicleStage)
+
