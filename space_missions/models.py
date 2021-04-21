b330a5b8a8ac0f33d9a4f0f0ef2ed23fed0a3ad2
eferos93 <eros.fabrici@gmail.com>
2021-04-21 14:32:30 +0200

    remove unused import

--- space_missions/models.py
+++ space_missions/models.py
@@ -1,7 +1,6 @@
-from typing import Sequence
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.translation import gettext_lazy as _
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
