f1ef1668eeed01d546b7c37e0b0c4bb035c8888a
Eros Fabrici <eros.fabrici@gmail.com>
2021-04-29 11:15:22 +0200

    updated settings and dockerignore

--- ODMC_exam_implementation/settings.py
+++ ODMC_exam_implementation/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', default='afdbkbwefwjvckjvb32534')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
